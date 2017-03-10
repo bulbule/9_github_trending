2152b0c9d9966f3431dd065f56bba17e00bc8027
bulbule <anyagoremykina@gmail.com>
2017-03-10 23:39:22 +0100

    first attempt

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,45 @@
+import datetime as DT
+import requests
+
+TOP_SIZE = 20
+DAYS_AGO = 7
+
+
+def starting_date(days_ago):
+    today = DT.date.today()
+    starting_date = today - DT.timedelta(days=days_ago)
+    return starting_date
 
 
 def get_trending_repositories(top_size):
-    pass
+
+    url = 'https://api.github.com/search/repositories'
+    searching_params = {'q': 'created:>={}'.format(starting_date(DAYS_AGO)),
+                        'sort': 'stars'
+                        }
+    r = requests.get(url, searching_params)
+    return r.json()['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+
+    url = 'https://api.github.com/{}/{}/issues'.format(repo_owner, repo_name)
+    r = requests.get(url)
+    return len(r.json())
+
+
+def print_repos(repos):
+    for repo in repos:
+        print(
+            'Name:{}\nurl:{}\nOpen issues:{}\n'.format(
+                repo['name'],
+                repo['url'],
+                get_open_issues_amount(
+                    repo['owner'],
+                    repo['name'])))
+
 
 if __name__ == '__main__':
-    pass
+
+    repos = get_trending_repositories(TOP_SIZE)
+    print_repos(repos)
